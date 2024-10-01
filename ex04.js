092be4f0fae1ca90c1d3fdf3a1df26c55b641491
Izushi <shoeshinesamurai@gmail.com>
2024-10-01 12:00:22 -0700

    ex04.js is done

--- ex04.js
+++ ex04.js
@@ -2,4 +2,25 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
+function mostFrequentChar(string) {
+  const letterCountObj = {};
+  let mostFrequentLetter = "";
+  let maxLetterCount = 0;
+
+  for (const letter of string) {
+    if (letterCountObj[letter]) {
+      letterCountObj[letter]++;
+    } else {
+      letterCountObj[letter] = 1;
+    }
+
+    if (letterCountObj[letter] > maxLetterCount) {
+      mostFrequentLetter = letter;
+      maxLetterCount = letterCountObj[letter];
+    }
+  }
+
+  return mostFrequentLetter;
+}
+
 console.log(mostFrequentChar("javascript")); // Expected output: "a"
