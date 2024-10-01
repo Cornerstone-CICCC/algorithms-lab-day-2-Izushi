0ab1df637d4c7a9922deb2a7a1b91708803391b3
Izushi <shoeshinesamurai@gmail.com>
2024-10-01 11:59:58 -0700

    ex01.js is done

--- ex01.js
+++ ex01.js
@@ -2,4 +2,12 @@
 // with the first letter of each word capitalized.
 // Example: capitalizeWords("hello world") should return "Hello World".
 
+function capitalizeWords(string) {
+  const wordsArr = string.split(" ");
+  const capitalizedWordsArr = wordsArr.map(word => {
+    return `${word[0].toUpperCase()}${word.slice(1)}`
+  });
+  return capitalizedWordsArr.join(" ");
+}
+
 console.log(capitalizeWords("hello world")); // Expected output: "Hello World"
