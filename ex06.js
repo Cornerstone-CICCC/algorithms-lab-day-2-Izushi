94c464a3fa7f094122f656406a3e9c125e8cdab9
Izushi <shoeshinesamurai@gmail.com>
2024-10-01 12:18:19 -0700

    ex06.js is done

--- ex06.js
+++ ex06.js
@@ -3,4 +3,15 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+function getQueryParams(url) {
+  let queryParams = {};
+  const urlParams = url.substring(url.indexOf("?") + 1);
+  const queryParamsArr = urlParams.split("&");
+  queryParamsArr.forEach(queryParam => {
+    let keyAndValue = queryParam.split("=");
+    queryParams[keyAndValue[0]] = keyAndValue[1];
+  });
+  return queryParams;
+}
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
